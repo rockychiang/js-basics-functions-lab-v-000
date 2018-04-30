2c96532564d95442fadf88c50fa843dee22db6dc
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-30 23:24:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function calculatesFarePrice(start, destination) {
   if (distance < 400) {
     return 0
   } else if (distance > 400 && distance < 2000) {
-    return distance - 400 * 
+    return (distance - 400) * 0.02
   }
 }
