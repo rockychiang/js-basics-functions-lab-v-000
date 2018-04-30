12fb6fc88a6e1095279e1318338e050f1afb8723
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-30 23:25:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  distance = location -42
+  return Math.abs(distance)
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  distance = (destination - start) * 264
+  return Math.abs(distance)
+}
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+  if (distance > 2500) {
+    return "cannot travel that far"
+  }
+  
+  if (distance < 400) {
+    return 0
+  } else if (distance > 400 && distance < 2000) {
+    return (distance - 400) * 0.02
+  } else {
+    return 25
+  }
+}
